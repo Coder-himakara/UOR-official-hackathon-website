444839014b08ae6fd185e3ddcac59eb38e9706b2
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2023-12-07 23:54:22 +0530

    Added some files
    Added index page and contact us age to views. Added a controller. Added routes.

--- Officail-Site/routes/web.php
+++ Officail-Site/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\hackathonController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[hackathonController::class,'index'])->name('index');
+Route::get('/contactus',[hackathonController::class,'contact'])->name('contactus');
