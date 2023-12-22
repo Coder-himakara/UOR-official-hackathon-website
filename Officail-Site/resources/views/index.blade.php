b343b1e6b7c59c10a1f21bad0d92864a5104264b
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2023-12-22 20:10:47 +0530

    Font families changed

--- Officail-Site/resources/views/index.blade.php
+++ Officail-Site/resources/views/index.blade.php
@@ -13,6 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Candal&family=Days+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Archivo&family=Black+Ops+One&family=Carter+One&family=Exo+2&family=Montserrat+Alternates:wght@800&family=Viga&display=swap"
+        rel="stylesheet">
+
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap CSS v5.2.1 -->
@@ -368,6 +374,7 @@
                 </div>
             </div>
         </section>
+        <!-- -->
         <script type="text/JavaScript" src="{{ asset('js/shakerobo.js') }}"></script>
     </main>
 
