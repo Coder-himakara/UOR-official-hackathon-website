68cb13a13adaeae653e7771da856d57d1206c8a0
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-01-06 07:15:33 +0530

    Changed the hackathaon logo

--- Officail-Site/resources/views/Reg-page.blade.php
+++ Officail-Site/resources/views/Reg-page.blade.php
@@ -34,7 +34,7 @@
             <div class="container-fluid px-0 px-md-1">
                 <a class="navbar-brand ms-4 me-0" href="{{ route('index') }}">
                     <!--Hackthon LOGO. Change with requirements-->
-                    <img src="{{ asset('images/logo 2.png') }}" border-radius: 278.50px width="auto" height="100px"
+                    <img src="{{ asset('images/logo without bg2.png') }}" border-radius: 278.50px width="auto" height="100px"
                         alt="Logo" class="vector_pos" id="logo" style="position:absolute; "
                         class="d-inline-block align-text-top">
                 </a>
