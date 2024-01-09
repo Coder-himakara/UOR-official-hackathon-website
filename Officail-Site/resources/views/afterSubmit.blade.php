7d5003a540fcc5e7456ce66b9e4e34be54b48299
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-01-09 18:25:08 +0530

    changed after submit

--- Officail-Site/resources/views/afterSubmit.blade.php
+++ Officail-Site/resources/views/afterSubmit.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!--Google Font link-->
+    <!-- Google Font link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Candal&family=Days+One&display=swap" rel="stylesheet">
@@ -18,53 +18,151 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!--Don't know why added. But don't delete-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="{{asset('css/afterSubmit.css')}}">
 
-    <!-- Custom Script -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">   
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+
+        .Thanktext,
+        .luck,
+        .pdf-download-text,
+        .f-contact,
+        .cpyright {
+            margin-bottom: 20px;
+        }
+
+        .f-logo li {
+            display: inline-block;
+            margin-right: 10px;
+        }
+
+        .footer {
+            padding-top: 20px;
+        }
+
+        /* Adjusted styles for the Thanktext */
+        .Thanktext span {
+            display: block;
+            font-size: 5vw;
+            /* Adjust as needed */
+        }
+
+        .luck h4 {
+            font-size: 3vw;
+            /* Adjust as needed */
+        }
+
+        .pdf-download-text p.lead,
+        .pdf-download-text a.btn {
+            font-size: 2vw;
+            /* Adjust as needed */
+        }
+
+        .cpyright {
+            font-size: 1.5vw;
+            /* Adjust as needed */
+        }
+
+        @media (max-width: 767px) {
+
+            /* Adjusted styles for smaller screens */
+            .f-logo li {
+                margin-right: 0;
+            }
+
+            .f-logo a {
+                display: block;
+                margin-bottom: 10px;
+            }
+
+            .f-contact li {
+                margin-bottom: 10px;
+            }
+        }
+
+        @media (min-width: 768px) and (max-width: 991px) {
+
+            /* Adjusted styles for medium screens */
+            .Thanktext span {
+                font-size: 4vw;
+            }
+
+            .luck h4 {
+                font-size: 2.5vw;
+            }
+
+            .pdf-download-text p.lead,
+            .pdf-download-text a.btn {
+                font-size: 1.8vw;
+            }
+
+            .cpyright {
+                font-size: 1vw;
+            }
+        }
+
+        @media (min-width: 992px) {
+
+            /* Default styles for larger screens */
+            .Thanktext span {
+                font-size: 5vw;
+            }
+
+            .luck h4 {
+                font-size: 3vw;
+            }
+
+            .pdf-download-text p.lead,
+            .pdf-download-text a.btn {
+                font-size: 2vw;
+            }
+
+            .cpyright {
+                font-size: 1.5vw;
+            }
+        }
+    </style>
 </head>
 
 <body>
-    <div class="Thanktext">
-        <h2>Thank you for registering with us!</h2>
-    </div>
-    <div class="luck">
-        <h4>"Best of luck ! Code your way to victory in the hackathon!"</h4>
-    </div>
-    <img src="{{asset('images/afterSubmit/logo background.png')}}" alt="logo" id=logobg>
-    <img src="{{asset('images/afterSubmit/design.svg')}}" alt="Design" id="d1" class="img-fluid vector_pos">
-
     <div class="container">
+        <div class="Thanktext text-center">
+            <span class="st8_1">Thank You For Registering With Us...!</span>
+        </div>
+    </div>
+
+    <div class="luck text-center">
+        <h4 class="display-6">"Best of luck! Code your way to victory in the hackathon!"</h4>
+    </div>
+
+    <div class="container text-center">
         <div class="pdf-download-text">
-            <p>Download your team details from below</p>
-        </div>
-        <div>
-            <a class="btn btn-primary" id="pdfbtn" href="{{ route('generate-pdf', ['id' => $Hackteam->id]) }}" target="_blank"
-                role="button">Download PDF</a>
+            <p class="lead">Download your team details from below</p>
+            <a class="btn btn-primary btn-lg" id="pdfbtn" href="{{ route('generate-pdf', ['id' => $Hackteam->id]) }}"
+                target="_blank" role="button">Download PDF</a>
         </div>
     </div>
-    <div class="container2">
-        <div class="return-home-text">
-            <p>Return to Home Page</p>
-        </div>
-        <div>
-            <a class="btn btn-primary" id="homebtn" href="{{ route('index') }}" role="button">Home Page</a>
+
+    <div class="container text-center">
+        <div class="pdf-download-text">
+            <p class="lead">Return to Home Page</p>
+            <a class="btn btn-primary btn-lg" id="homebtn" href="{{ route('index') }}" role="button">Home Page</a>
         </div>
     </div>
 
 
-
-
-    <!-- Footer-->
+    <!-- Footer -->
     <footer class="footer">
         <div class="container-fluid px-lg-5">
             <div class="row">
-                <!-- Footer Left-->
+                <!-- Footer Left -->
                 <div class="col-lg-6 col-md-6 col-sm-12 lfooter">
+                    <!-- Address -->
                     <span><i class="fa fa-home"></i></span><span class="ms-2">UNIVERSITY OF RUHUNA,<br>
                         WELLAMADAMA, MATARA, <br>SRI LANKA.</span>
+                    <!-- Social Media links -->
                     <ul class="f-logo">
                         <li>
                             <a href=""><i class="fa fa-home mx-2"></i></a>
@@ -74,25 +172,23 @@
                     </ul>
                 </div>
 
-                <!-- Footer Right-->
-                <div class="col-lg-6 col-md-6 col-sm-12 text-justify-center rfooter">
+                <!-- Footer Right -->
+                <div class="col-lg-6 col-md-6 col-sm-12 text-md-start text-sm-center rfooter">
                     <!-- Social Media links -->
                     <ul class="f-contact">
                         <li>
-                            <!-- Facebook -->
                             <a href=""><i class="fa fa-facebook-f social me-2"></i></a>
-                            <!-- Twitter -->
                             <a href=""><i class="fa fa-twitter social mx-2"></i></a>
-                            <!-- Google + -->
                             <a href=""><i class="fa fa-youtube social mx-2"></i></a>
-                            <!-- Linkedin -->
                             <a href=""><i class="fa fa-linkedin social mx-2"></i></a>
                         </li>
                         <li>
+                            <!-- Email -->
                             <span><i class="fa fa-envelope"></i></span><span
                                 class="ms-lg-2">Insurgex.ruh@gmail.com</span>
                         </li>
                         <li>
+                            <!-- Phone -->
                             <span><i class="fa fa-phone"></i></span><span class="ms-2">+94 702392322</span>
                         </li>
                     </ul>
@@ -100,14 +196,16 @@
             </div>
         </div>
         <div class="container text-center">
-            <!-- Footer Copyrigth-->
+            <!-- Footer Copyright -->
             <small class="lead mb-0 cpyright">&copy; 2023 Copyright Inc. All rights reserved.</small>
         </div>
     </footer>
+
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
         integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
