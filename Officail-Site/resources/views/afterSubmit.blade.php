07f29e30e6b725f1d1bbc552d69024832c43750c
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-01-11 21:14:00 +0530

    AfterSubmit page added

--- Officail-Site/resources/views/afterSubmit.blade.php
+++ Officail-Site/resources/views/afterSubmit.blade.php
@@ -18,11 +18,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{asset('css/afterSubmit.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     <style>
+        header {
+            background-color: black;
+        }
+
         body {
             overflow-x: hidden;
+        }
+
+        .shw_vwport {
+            padding-top: 7%;
         }
 
         .Thanktext,
@@ -30,7 +38,7 @@
         .pdf-download-text,
         .f-contact,
         .cpyright {
-            margin-bottom: 20px;
+            margin-bottom: 30px;
         }
 
         .f-logo li {
@@ -43,27 +51,36 @@
         }
 
         /* Adjusted styles for the Thanktext */
-        .Thanktext span {
+        .Thanktext p {
             display: block;
-            font-size: 5vw;
-            /* Adjust as needed */
+            font-size: 4vw;
         }
 
         .luck h4 {
             font-size: 3vw;
-            /* Adjust as needed */
         }
 
         .pdf-download-text p.lead,
         .pdf-download-text a.btn {
             font-size: 2vw;
-            /* Adjust as needed */
         }
 
         .cpyright {
             font-size: 1.5vw;
-            /* Adjust as needed */
         }
+        
+        .images_relative{
+            position: relative;
+        }
+        #rimg1{
+            animation: fade-up 0.5s 0.4s backwards;
+            width: 350px;
+            position: absolute;
+            right: 0px;
+            top: -177px;
+        }
+        
+
 
         @media (max-width: 767px) {
 
@@ -127,42 +144,59 @@
 </head>
 
 <body>
-    <div class="container">
+
+    <header class="header">
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+            <div class="container-fluid px-0 px-md-1">
+                <a class="navbar-brand ms-4 me-0" href="index.html">
+                    <!-- Hackathon LOGO. Change with requirements -->
+                    <img src="{{asset('images/logo 2.png')}}" border-radius: 278.50px width="auto" height="100px" alt="Logo"
+                        class="vector_pos" id="logo" style="position:absolute; " class="d-inline-block align-text-top">
+                </a>
+            </div>
+        </nav>
+    </header>
+    <div class="container shw_vwport">
+
         <div class="Thanktext text-center">
-            <span class="st8_1">Thank You For Registering With Us...!</span>
+            <p class="thank_you" style="font-size: 30px;">Thank You For Registering With Us...!</p>
+        </div>
+
+
+        <div class="luck text-center">
+            <h4 class="display-6">"Best of luck! Code your way to victory in the hackathon!"</h4>
+        </div>
+
+        <div class=" text-center">
+            <div class="pdf-download-text">
+                <p class="lead">Download your team details from below</p>
+                <a href="{{ route('generate-pdf', ['id' => $Hackteam->id]) }}" target="_blank"><button type="button" class="btn btn-primary bttn"><span class="bttn-text">Download
+                            PDF</span></button></a>
+            </div>
+        </div>
+
+        <div class="images_relative">
+            <img src="{{asset('images/Human_vector.png')}}" alt="Human"  id="rimg1">
+        </div>
+
+
+        <div class="text-center">
+            <div class="pdf-download-text">
+                <p class="lead">Return to Home Page</p>
+                <a href="{{route('index')}}"><button type="button" class="btn btn-primary bttn"><span class="bttn-text">Home
+                            Page</span></button></a>
+            </div>
         </div>
     </div>
-
-    <div class="luck text-center">
-        <h4 class="display-6">"Best of luck! Code your way to victory in the hackathon!"</h4>
-    </div>
-
-    <div class="container text-center">
-        <div class="pdf-download-text">
-            <p class="lead">Download your team details from below</p>
-            <a class="btn btn-primary btn-lg" id="pdfbtn" href="{{ route('generate-pdf', ['id' => $Hackteam->id]) }}"
-                target="_blank" role="button">Download PDF</a>
-        </div>
-    </div>
-
-    <div class="container text-center">
-        <div class="pdf-download-text">
-            <p class="lead">Return to Home Page</p>
-            <a class="btn btn-primary btn-lg" id="homebtn" href="{{ route('index') }}" role="button">Home Page</a>
-        </div>
-    </div>
-
-
-    <!-- Footer -->
+    <!-- Footer-->
     <footer class="footer">
         <div class="container-fluid px-lg-5">
             <div class="row">
-                <!-- Footer Left -->
-                <div class="col-lg-6 col-md-6 col-sm-12 lfooter">
-                    <!-- Address -->
-                    <span><i class="fa fa-home"></i></span><span class="ms-2">UNIVERSITY OF RUHUNA,<br>
-                        WELLAMADAMA, MATARA, <br>SRI LANKA.</span>
-                    <!-- Social Media links -->
+                <!-- Footer Left-->
+                <div class="col-lg-7 col-md-7 col-sm-12 lfooter text-center">
+                    <span><i class="fa fa-home"></i></span><span class="ms-2">UNIVERSITY OF RUHUNA,
+                        WELLAMADAMA<br>MATARA, SRI LANKA.</span>
                     <ul class="f-logo">
                         <li>
                             <a href=""><i class="fa fa-home mx-2"></i></a>
@@ -172,23 +206,20 @@
                     </ul>
                 </div>
 
-                <!-- Footer Right -->
-                <div class="col-lg-6 col-md-6 col-sm-12 text-md-start text-sm-center rfooter">
+                <!-- Footer Right-->
+                <div class="col-lg-5 col-md-5 col-sm-12 text-justify-center rfooter">
                     <!-- Social Media links -->
                     <ul class="f-contact">
                         <li>
-                            <a href=""><i class="fa fa-facebook-f social me-2"></i></a>
-                            <a href=""><i class="fa fa-twitter social mx-2"></i></a>
-                            <a href=""><i class="fa fa-youtube social mx-2"></i></a>
-                            <a href=""><i class="fa fa-linkedin social mx-2"></i></a>
+                            <!-- Facebook -->
+                            <a href=""><i class="fa fa-facebook-f social"></i></a><span
+                                class="ms-lg-2">www.facebook.com</span>
                         </li>
                         <li>
-                            <!-- Email -->
                             <span><i class="fa fa-envelope"></i></span><span
-                                class="ms-lg-2">insurgex.ruh@gmail.com</span>
+                                class="ms-lg-2">Insurgex.ruh@gmail.com</span>
                         </li>
                         <li>
-                            <!-- Phone -->
                             <span><i class="fa fa-phone"></i></span><span class="ms-2">+94 702392322</span>
                         </li>
                     </ul>
@@ -196,7 +227,7 @@
             </div>
         </div>
         <div class="container text-center">
-            <!-- Footer Copyright -->
+            <!-- Footer Copyrigth-->
             <small class="lead mb-0 cpyright">&copy; 2023 Copyright Inc. All rights reserved.</small>
         </div>
     </footer>
@@ -204,22 +235,20 @@
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
         integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-
+        </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+        </script>
+
+    <script type="text/JavaScript" src="main.js"></script>
+
+    <script>
+        history.pushState(null, null, location.href);
+        window.onpopstate = function () {
+            history.pushState(null, null, location.href);
+        };
     </script>
-    <!-- Custom JavaScript -->
-    <script type="text/JavaScript" src="{{ asset('js/main.js') }}"></script>
 
 </body>
-
-<!--To disable go back feature. But it is not working in every case-->
-<script>
-    history.pushState(null, null, location.href);
-    window.onpopstate = function() {
-        history.pushState(null, null, location.href);
-    };
-</script>
 
 </html>
