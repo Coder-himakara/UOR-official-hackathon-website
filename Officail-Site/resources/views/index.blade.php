ffe645c8037b8a13454a2376e5de80e8234cd258
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-01-13 01:55:43 +0530

    spelling change

--- Officail-Site/resources/views/index.blade.php
+++ Officail-Site/resources/views/index.blade.php
@@ -143,7 +143,7 @@
 
                 </div>
                 <div class="row">
-                    <p class="text-center countdown_title  col-md-6">Registration will be closed by</p>
+                    <p class="text-center countdown_title  col-md-6">Registration will be closed on</p>
                     <div class="countdown-container">
                         <div>
                             <p id="days" class="big-text">0</p>
